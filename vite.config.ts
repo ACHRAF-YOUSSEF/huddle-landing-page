e3ee10b0f0cae3d41b52c9b389300ee5ab5a6756
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2023-06-01 18:14:27 +0100

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/huddle-landing-page-with-a-single-ntroductory-section-frontend-mentor/'
+  base: '/huddle-landing-page/'
 })
